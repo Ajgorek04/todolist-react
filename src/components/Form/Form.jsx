86012fe88deb9067a066bf5591571a119baa94dd
Ajgorek <igorblendzinski12@gmail.com>
2023-11-12 23:43:17 +0100

    Adding input value on change

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import styles from "./Form.module.css";
 
 export function Form() {
+    const [inputValue, setInputValue] = useState("");
+
+    const handleButtonClick = (e) => {
+        e.preventDefault();
+
+        if (inputValue.trim() !== "") {
+            console.log(inputValue);
+            setInputValue("");
+        } else {
+            console.error("Pole nie może być puste");
+        }
+    };
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
     return (
         <form className={styles.form}>
-            <input type="text" placeholder="Task Name" />
-            <button> Add </button>
+            <input
+                type="text"
+                placeholder="Task Name"
+                value={inputValue}
+                onChange={handleInputChange}
+            />
+            <button onClick={handleButtonClick}> Add </button>
         </form>
     );
 }
