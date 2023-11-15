40d4154da34065884693daf27f6d71cf1e43ea0b
Ajgorek <igorblendzinski12@gmail.com>
2023-11-16 00:45:21 +0100

    adding styles to list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,7 +4,13 @@ export function List({ tasks }) {
     return (
         <ul className={styles.ulList}>
             {tasks.map((task, index) => (
-                <li key={index}>{task}</li>
+                <li key={index}>
+                    <p>{task}</p>
+                    <div className={styles.buttons}>
+                        <button>DEL</button>
+                        <button>EDIT</button>
+                    </div>
+                </li>
             ))}
         </ul>
     );
